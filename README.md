1147a6000d7a9152331fa505b7c5a35422ce572d
Warren MacEvoy <wmacevoy@gmail.com>
2020-05-11 08:49:44 -0700

    linux support

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bitcoin-portable
 run bitcoin from any directory and from windows/mac/linux
+
+./setup - configure (osx only for now)
+
+./bitcoin - launch bitcoin with data directory of ./data
+
--- config.sh
+++ config.sh
@@ -4,7 +4,14 @@ os="`uname`"
 if [ "$os" = "Darwin" ]
 then
     BITCOIN_CORE_OS=osx64
+elif [ "$os" = "Linux" ]
+then
+    BITCOIN_CORE_OS=x86_64-linux-gnu
+else
+    echo "unknown os."
+    exit 1
 fi
 
 BITCOIN_CORE_VERSION=0.19.1
 BITCOIN_CORE_URL=https://bitcoin.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.tar.gz
+BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt"
