9d2fec2fb51fe1a9d8f8d649f37465599cd80a2a
Warren MacEvoy <wmacevoy@gmail.com>
2022-08-23 15:19:05 -0600

    osx fix

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 os="`uname`"
 if [ "$os" = "Darwin" ]
 then
-    BITCOIN_CORE_OS=osx64
+    BITCOIN_CORE_OS=x86_64-apple-darwin
 elif [ "$os" = "Linux" ]
 then
     BITCOIN_CORE_OS=x86_64-linux-gnu
@@ -14,4 +14,5 @@ fi
 
 BITCOIN_CORE_VERSION=23.0
 BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.tar.gz
+BITCOIN_CORE_URL_SHA256=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/SHA256SUMS
 BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt"
