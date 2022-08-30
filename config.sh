404929df197a1787fcffb3b0ba869885c2ab6ed2
Warren MacEvoy <wmacevoy@gmail.com>
2022-08-29 22:47:04 -0600

    win64

--- config.sh
+++ config.sh
@@ -6,13 +6,32 @@ then
     BITCOIN_CORE_OS=x86_64-apple-darwin
 elif [ "$os" = "Linux" ]
 then
-    BITCOIN_CORE_OS=x86_64-linux-gnu
-else
-    echo "unknown os."
-    exit 1
+    if grep -q Microsoft /proc/version
+    then
+        BITCOIN_CORE_OS=win64
+    else
+        BITCOIN_CORE_OS=x86_64-linux-gnu
+    fi
+elif [[ "$os" =~ ^MINGW ]]
+then
+    BITCOIN_CORE_OS=win64
 fi
 
+
 BITCOIN_CORE_VERSION=23.0
-BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.tar.gz
+
+if [ $BITCOIN_CORE_OS != "win64" ]
+then
+    BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.tar.gz
+else
+    BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.zip
+fi
+
 BITCOIN_CORE_URL_SHA256=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/SHA256SUMS
-BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt"
+
+if [ $BITCOIN_CORE_OS != "win64" ]
+then
+    BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt"
+else
+    BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt.exe"
+fi
