80bea8ceec43c45877e6d7a5a5a60323f7578a43
Warren MacEvoy <wmacevoy@gmail.com>
2022-09-21 11:24:09 -0600

    arm trial

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 os="`uname`"
 if [ "$os" = "Darwin" ]
 then
-    BITCOIN_CORE_OS=x86_64-apple-darwin
+    BITCOIN_CORE_OS=$(uname -m)-apple-darwin
 elif [ "$os" = "Linux" ]
 then
     if grep -q Microsoft /proc/version
     then
         BITCOIN_CORE_OS=win64
     else
-        BITCOIN_CORE_OS=x86_64-linux-gnu
+        BITCOIN_CORE_OS=$(uname -m)-linux-gnu
     fi
 elif [[ "$os" =~ ^MINGW ]]
 then
@@ -23,19 +23,14 @@ BITCOIN_CORE_VERSION=23.0
 if [ $BITCOIN_CORE_OS != "win64" ]
 then
     BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.tar.gz
+    BITCOIN_CORE_EXTENSION=""
 else
     BITCOIN_CORE_URL=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS.zip
+    BITCOIN_CORE_EXTENSION=".exe"
 fi
 
 BITCOIN_CORE_URL_SHA256=https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_CORE_VERSION/SHA256SUMS
 
-if [ $BITCOIN_CORE_OS != "win64" ]
-then
-    BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt"
-    BITCOIND_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoind"
-    BITCOIN_CLI_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-cli"    
-else
-    BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt.exe"
-    BITCOIND_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoind.exe"    
-    BITCOIN_CLI_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-cli.exe"
-fi
+BITCOIN_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-qt$BITCOIN_CORE_EXTENSION"
+BITCOIND_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoind$BITCOIN_CORE_EXTENSION"
+BITCOIN_CLI_CORE_EXECUTABLE="$at/bitcoin-$BITCOIN_CORE_VERSION-$BITCOIN_CORE_OS/bin/bitcoin-cli$BITCOIN_CORE_EXTENSION"
